009e305f36b9ac33c637f9e2fd0f960e2ea40352
duarte.a.arribas@gmail.com <duarte.a.arribas@gmail.com>
2023-01-02 04:56:01 +0000

    added better printing

--- main.py
+++ main.py
@@ -20,15 +20,30 @@ def getListOfAcronymsWithoutAc(file,acronyms):
         for acronym in acronyms:
           if acronym in word:
             if all([acronymPrefix not in word for acronymPrefix in ["\\ac","\\Ac"]]) and "\\label" not in word and line.strip()[0] != "%":
-              acronymsWithoutAc.append((acronym,lineCount))
+              acronymsWithoutAc.append((acronym,lineCount,line))
       lineCount += 1
-    return acronymsWithoutAc  
+    return acronymsWithoutAc
+    
+def printListOfAcronymsWithoutAc(acronymsWithoutAc):
+  if len(acronymsWithoutAc):
+    print(f"There are {len(acronymsWithoutAc)} acronyms not marked as acronyms in the document.")
+  else:
+    print(f"All the acronyms are marked. Good Job!")
+    return
+  print("\n\n")
+  for acronymWithoutAc in acronymsWithoutAc:
+    print(f"| Acronym '{acronymWithoutAc[0]}' on line {acronymWithoutAc[1]} |\n{acronymWithoutAc[2]}")
+    print(" " * acronymWithoutAc[2].index(acronymWithoutAc[0]),end='')
+    print("^")
+    print(" " * acronymWithoutAc[2].index(acronymWithoutAc[0]),end='')
+    print("|")
 
 def main():
   if len(sys.argv) != 3:
     print("Usage: python main.py fileToCheck acronymFile")
     sys.exit(-1)
-  print(getListOfAcronymsWithoutAc(sys.argv[1],getAcronymList(sys.argv[2])))
+  acronymsWithoutAc = getListOfAcronymsWithoutAc(sys.argv[1],getAcronymList(sys.argv[2]))
+  printListOfAcronymsWithoutAc(acronymsWithoutAc)
 
 if __name__ == "__main__":
   main()
