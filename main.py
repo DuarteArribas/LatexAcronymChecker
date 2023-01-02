929617c6e72425ab4d7a741542af9dab5dc57646
duarte.a.arribas@gmail.com <duarte.a.arribas@gmail.com>
2023-01-02 00:40:21 +0000

    added get acronyms from acronym document

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import sys
 
+def getAcronymList(acronymFile):
+  with open(acronymFile,"r") as f:
+    lines = f.readlines()
+    listOfAcronyms = []
+    for line in lines:
+      if "\\acro" in line.strip():
+        listOfAcronyms.append(line.split("{")[1].strip("}"))
+    return listOfAcronyms
+
+
+#def getListOfAcronymsWithoutAc(file):
+#  with open(file,"r") as f:
+#    lines = f.readlines()
+#    acronymsWithoutAc = []
+#    for line in lines:
+#      for word in line.split(" "):
+#        
+#    
+#    return acronymsWithoutAc  
 
 def main():
-  if len(sys.argv) != 2:
-    print("Usage: python main.py fileToCheck")
+  if len(sys.argv) != 3:
+    print("Usage: python main.py fileToCheck acronymFile")
     sys.exit(-1)
-  print(sys.argv[1])
-  #print(getListOfAcronymsWithoutAc(sys.argv[1]))
+  print(getAcronymList(sys.argv[2]))
 
 if __name__ == "__main__":
   main()
