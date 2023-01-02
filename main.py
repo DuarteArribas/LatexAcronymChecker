2f87dd7e9adffac842833cfa8225d00dfe79bd55
duarte.a.arribas@gmail.com <duarte.a.arribas@gmail.com>
2023-01-02 00:53:44 +0000

    added line number to error checking

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ def getListOfAcronymsWithoutAc(file,acronyms):
   with open(file,"r") as f:
     lines = f.readlines()
     acronymsWithoutAc = []
+    lineCount = 1
     for line in lines:
       for word in line.split(" "):
         for acronym in acronyms:
           if acronym in word:
             if all([acronymPrefix not in word for acronymPrefix in ["\\ac","\\Ac"]]) and "\\label" not in word:
-              print(word)
-        
-    
+              acronymsWithoutAc.append((acronym,lineCount))
+      lineCount += 1
     return acronymsWithoutAc  
 
 def main():
